2e05ce889e9c7a8914fa97068e244131ebc8d14e
Ultramarine000 <zhanghuajing588@gmail.com>
2023-04-18 13:07:40 +0100

    timeBonusFix

--- Assets/Scripts/Patrol.cs
+++ Assets/Scripts/Patrol.cs
@@ -7,7 +7,7 @@ public class Patrol : MonoBehaviour
 {
     public float speed = 20f;
     public GameObject[] wayPoint;
-    int nextPointIndex;
+    [SerializeField]int nextPointIndex;
     void Start()
     {
         //wayPoint = GameObject.FindGameObjectsWithTag("WayPoint1_Blue");
@@ -23,7 +23,7 @@ public class Patrol : MonoBehaviour
     void Update()
     {
         //distance to next way point
-        if (Vector3.Distance(wayPoint[nextPointIndex].transform.position, transform.position) < 0.1f)
+        if (Vector3.Distance(wayPoint[nextPointIndex].transform.position, transform.position) < 0.2f)
         {
             // if the next way point is not the last then +1
             if (nextPointIndex != wayPoint.Length - 1)
@@ -31,7 +31,7 @@ public class Patrol : MonoBehaviour
                 nextPointIndex++;
             }
             //reach the last position, position fixed to avoid shaking
-            if (Vector3.Distance(wayPoint[wayPoint.Length - 1].transform.position, transform.position) < 0.1f)
+            if (Vector3.Distance(wayPoint[wayPoint.Length - 1].transform.position, transform.position) < 0.2f)
             {
                 //transform.position = wayPoint[wayPoint.Length - 1].transform.position;
                 nextPointIndex = 0;
